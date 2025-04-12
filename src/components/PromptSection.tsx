a288d9a27e45e06f86bfce75e4f27b0ae7179f29
Jordan Umusu <jordanumusu@gmail.com>
2025-04-12 17:52:05 +0200

    Monorepo setup for backend

--- src/components/PromptSection.tsx
+++
@@ -1,9 +0,0 @@
-import { PromptForm } from "@/components/PromptForm";
-export function PromptSection() {
-  return (
-    <div className="flex flex-col h-full w-full p-4 items-center">
-      <div className="flex-1 overflow-y-auto"></div>
-      <PromptForm />
-    </div>
-  );
-}
